8fdd0a9b175d0e2229d12b4740900570037a0bbd
Sarthak Singh <singhsarthak07072006@gmail.com>
2026-09-13 00:14:50 +0530

    add Tailwind CSS integration and implement basic layout with Header and Footer components; set up Redux for authentication state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,44 @@
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth.js";
+import { login, logout } from "./store/authSlice.js";
+import { Header, Footer } from "./components/index.js";
+import { Outlet } from "react-router-dom";
+
 function App() {
-    return <></>;
+    const [loading, setLoading] = useState(true);
+    const dispatch= useDispatch();
+
+    useEffect(() => {
+        authService.getCurrentUser()
+        .then((data) => {
+                dispatch(login({userData: data}));
+        })
+        .catch((error) => {
+            dispatch(logout());
+        })
+        .finally(() => setLoading(false));
+    }, []);
+
+    // conditional rendering based on loading state
+    return loading ? (
+        <div></div>
+    ) : (
+        <div
+            className="min-h-screen flex flex-wrap content-between bg-gray-400"
+        >
+            <div
+                className="w-full block"
+            >
+                <Header />
+                <main>
+                    TODO Outlet
+                    {/* <Outlet /> */}
+                </main>
+                <Footer />
+            </div>
+        </div>
+    );
 }
 
 export default App;
