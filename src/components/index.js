677b6d651d6b6a4c0d4cb0b03c8a599ec6561c20
Sarthak Singh <singhsarthak07072006@gmail.com>
2026-09-13 18:33:22 +0530

    update index.js to export new components: Button, Input, Select, PostCard, Login, and Signup

--- src/components/index.js
+++ src/components/index.js
@@ -3,8 +3,15 @@ import Footer from "./Footer/Footer.jsx";
 import Container from "./Container/Container.jsx";
 import Logo from "./Logo.jsx";
 import LogoutBtn from "./Header/LogoutBtn.jsx";
+import Button from "./Button.jsx";
+import Select from "./Select.jsx";
+import PostCard from "./PostCard.jsx";
+import Login from "./Login.jsx";
+import Input from "./Input.jsx";
+import Signup from "./Signup.jsx";
 
 export {
     Header, Footer, Container,
-    Logo, LogoutBtn,
+    Logo, LogoutBtn, Button, Input,
+    Select, PostCard, Login, Signup
 };
